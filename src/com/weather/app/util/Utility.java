7fedf0b61bb5a089eedc01a24aba7d9005706016
Mage <mouyongzhao@mouyongzhao.tricheer.com>
2015-09-29 14:18:19 +0800

    新增切换城市和手动更新天气的功能

--- src/com/weather/app/util/Utility.java
+++ src/com/weather/app/util/Utility.java
@@ -103,7 +103,7 @@ public class Utility {
 			
 			SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
 			editor.putBoolean("city_selected", true);
-			editor.putString("city_Name", cityName);
+			editor.putString("city_name", cityName);
 			editor.putString("weatherCode", weatherCode);
 			editor.putString("temp1", temp1);
 			editor.putString("temp2", temp2);
